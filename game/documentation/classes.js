5f9259290a5e1c15f4a76aacc968d766b3c757c9
NemGame <hoakzo@gmail.com>
2025-09-21 19:00:50 +0200

    documentation

---
+++ game/documentation/classes.js
@@ -0,0 +1,65 @@
+class Stuff {
+    constructor(name="", tags=[], path="") {
+        this.name = name;
+        this.tags = typeof tags == "object" ? tags : [`${tags}`];
+        this.path = path == "" ? Stuff.stuffify(name) : path;
+    }
+    static get null() {
+        return new Stuff();
+    }
+    static stuffify(x="") {
+        return x.toLowerCase().replaceAll("á", "a")
+                .replaceAll("é", "e").replaceAll("í", "i")
+                .replaceAll("ó", "o").replaceAll("ö", "o")
+                .replaceAll("ő", "o").replaceAll("ú", "u")
+                .replaceAll("ü", "u").replaceAll("ű", "u").replaceAll(" ", "_");
+    }
+    get content() {
+        return fetch("./data/" + this.path + ".html").then(x => x.text()).then(x => {
+            return QuickObjectify(x, "div", "class=stuff");
+        })
+    }
+}
+
+function QuickObjectify(content="", inbed="div", ...attributes) {
+    let bed = document.createElement(inbed);
+    bed.innerHTML = content;
+    if (typeof attributes == "object")
+        attributes.forEach(attr => {
+            let data = attr.split("=")
+            bed.setAttribute(data[0], data[1] || "")
+        });
+    return bed;
+}
+
+function LoadLanguage(div=HTMLDivElement, dict={}, currentLang="hu") {
+    if (!currentLang in dict) {
+        console.error("Language not found in dict: " + currentLang);
+        return 1;
+    }
+    div.querySelectorAll("[text]").forEach(x => {
+        let text = x.getAttribute("text").toLowerCase();
+        if (text in dict[currentLang])
+            x.innerHTML = dict[currentLang][text];
+        else
+            x.innerHTML = text;
+    })
+}
+
+function GetDictFromLang(div=HTMLDivElement) {
+    let lang = div.querySelector("langs");
+    if (!lang) return {};
+    let dict = {};
+    [...lang.children].forEach(l => {
+        dict[l.localName] = {};
+        [...l.children].forEach(x => {
+            dict[l.localName][x.localName] = x.innerHTML;
+        });
+    });
+    return dict;
+}
+
+function ReloadWithLang(lang="") {
+    let x = window.location.pathname;
+    window.open(window.location.protocol + "//" + window.location.host + x + "?lang=" + lang, "_self");
+}
--- game/documentation/scripts.js
+++ game/documentation/scripts.js
@@ -1,11 +1,51 @@
 document.addEventListener("DOMContentLoaded", LateLoad);
 
+const astuff = [
+    new Stuff("Controls", "szupi-dupi cucc", "controls"),
+    new Stuff("Default", "stuff", "default")
+]
+
+let params = new URLSearchParams(window.location.search);
+
+let currentLanguage = params.get("lang") || "hu";
+
 function LateLoad() {
-    
+    ReloadFinder();
+
     Update();
 }
 
 function Update() {
     
     requestAnimationFrame(Update);
+}
+
+function LoadStuff(stuff=Stuff.null) {
+    const holder = document.querySelector(".stuffholder");
+    holder.innerHTML = "";
+    stuff.content.then(x => {
+        let dict = GetDictFromLang(x);
+        LoadLanguage(x, dict, currentLanguage);
+        holder.appendChild(x);
+    });
+}
+
+let lastSet = null;
+
+function ReloadFinder() {
+    const finder = document.querySelector(".finder");
+    finder.innerHTML = "";
+    let base = QuickObjectify("<p name></p>", "div", "class=finderStuff");
+    astuff.forEach((x, i) => {
+        let c = base.cloneNode(true);
+        c.querySelector("[name]").textContent = x.name;
+        c.setAttribute("bound", i);
+        c.addEventListener("click", () => {
+            if (lastSet != null) lastSet.classList.remove("current");
+            lastSet = c;
+            c.classList.add("current");
+            LoadStuff(astuff[i]);
+        })
+        finder.appendChild(c);
+    });
 }
