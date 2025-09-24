d2793e01229dc731ba178fd61181b37a6a29d8a0
NemGame <hoakzo@gmail.com>
2025-09-24 07:13:19 +0200

    Translation added for about us title, bugfix for about us: removed css for nav.css (id does not exist)

--- game/end/langs.js
+++ game/end/langs.js
@@ -1,10 +1,12 @@
 const langs = {
     "hu": {
+        "cim": "Nevek és feladatok:",
         "akos": "Játék megírása és ahhoz a dokumentáció, ötletelés, navigációs sáv és nyelvváltás megírása",
         "zsombor": "Játék rajzok készítése, ötletelés, a fő, az rólunk és az AAAA oldalak megírása",
         "benedek": "Professzionális információk gyűjtése életszerűbb játékélményért"
     },
     "en": {
+        "cim": "Names and tasks:",
         "akos": "Game development, ideas, navigation bar and language switching",
         "zsombor": "Game art, ideas, main, about us and AAAA pages",
         "benedek": "Professional information gathering for a more realistic game experience"
